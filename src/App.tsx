c3161182a75f941a74089a0e62830d8767143c08
Adam Rais <raisadamo@gmail.com>
2021-08-03 14:52:55 +0900

    Remove react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 import Menu from './components/menu';
 import Home from './pages/home';
 import Projects from './pages/projects';
@@ -11,21 +6,12 @@ import Contact from './pages/contact';
 
 const App = ():React.ReactElement => {
   return (
-    <Router>
+    <>
       <Menu />
-      
-      <Switch>
-        <Route path="/">
-          <Projects />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/contact">
-          <Contact/>
-        </Route>
-      </Switch>
-    </Router>
+      <Home />
+      <Projects />
+      <Contact/>
+    </>
   )
 }
 
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,18 +1,14 @@
 import React, { useState } from 'react';
-import { useHistory } from "react-router-dom";
 import { Ul } from './styles';
 
 const Menu = (): React.ReactElement => {
     const [prevTarget, setTarget] = useState<any>();
-    let history = useHistory();
 
     const onClick = ({target}: any): void => {        
         prevTarget && prevTarget.classList.remove("active");
         setTarget(target);        
         target.classList.add("active");
-        console.log(`/${target.dataset.label}`);
-        history.push(`/${target.dataset.label}`);
-    }
+    };
 
     return <>
         <Ul>
